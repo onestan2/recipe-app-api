91523788a59be2828a571a1ccd0f443ce6040385
Damon Bryant <damonbry@gmail.com>
2020-06-11 22:26:23 -0400

    Corrected Error in settings with db user variable

--- app/app/settings.py
+++ app/app/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
-        'USER': os.envirnoment.get('DB_USER'),
-        'PASSWORD': os.environment.get("DB_PASS")
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get("DB_PASS")
 
     }
 }
