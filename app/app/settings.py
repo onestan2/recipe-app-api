41cfdd92e5f8d2424825a6e7e1ce45d9cc9c2f8d
Damon Bryant <damonbry@gmail.com>
2020-06-14 02:25:13 -0400

    Created core and recipe apps with apis

--- app/app/settings.py
+++ app/app/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS += [
     'rest_framework.authtoken',
     'core',
     'user',
-
+    'recipe',
 ]
 
 MIDDLEWARE = [
--- app/core/admin.py
+++ app/core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 
-from . import models
+from core import models
 
 
 class UserAdmin(BaseUserAdmin):
@@ -27,3 +27,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Tag)
